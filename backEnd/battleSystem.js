89c039b66afd19c3d7624ec7f32f10ba75e3e1ed
SetsuDesu17 <setsudesu17@gmail.com>
2026-04-05 00:26:49 +0800

    Refactored backend (status: estimated 40% complete)

--- backEnd/battleSystem.js
+++ backEnd/battleSystem.js
@@ -1,34 +1,79 @@
 import skills from "./skills.js";
-import BattleScreen from "./battleScreen.js";
+import battleScreen from "./battleScreen.js";
 import PromptSync from "prompt-sync";
 
 const prompt = PromptSync({}); 
 
 const battleSystem = {
-    alphabetArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    
 
-    preBattle: (playerSpeed, enemySpeed) => {
+    skillList: [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ], // USELESS ASF
+    // 0 = skills.randStr(player, enemy, stringArray, intArray),
+    // 1 = skills.randInt1To5(player, enemy, stringArray, intArray),
+    // 2 = skills.strConcat( player, enemy, stringArray, intArray ),
+    // 3 = skills.strLength( player, enemy, stringArray, intArray ),
+    // 4 = skills.intToStr( player, enemy, stringArray, intArray ),
+    // 5 = skills.add( player, enemy, stringArray, intArray ),
+    // 6 = skills.multiply( player, enemy, stringArray, intArray ),
+    // 7 = skills.intDmg( player, enemy, stringArray, intArray ),
+    // 8 = skills.strDmg( player, enemy, stringArray, intArray ),
 
-        let turnOrder;
-        let maxStringCount = 5;
-        let maxIntCount = 5;
-        let randomStringArrayStarter = battleSystem.alphabetArray.sort(() => 0.5 - Math.random()).slice(0, maxStringCount);
-        let randomIntArrayStarter = Array.from({ length: maxIntCount }, () => Math.floor(Math.random() * 100));
-        if (playerSpeed > enemySpeed) {
-            turnOrder = ['player', 'enemy'];
-        } else {
-            turnOrder = ['enemy', 'player'];
+
+    preBattle: () => {
+        let maxStarterSkillCount = 5; // Maximum number of skills in the player's skill list starter
+        let playerSkillsArray = [];
+        for(let i = 0; i < maxStarterSkillCount; i++) {
+            let randSkill = Math.floor(Math.random() * (skills.skillNames.length)); // Randomly select a skill from the skills list
+            playerSkillsArray.push(randSkill); // Randomly add skills to the player's skill list starter, can contain duplicates
         }
-        const stringArray = randomStringArrayStarter;
-        const intArray = randomIntArrayStarter;
-        return [turnOrder, stringArray, intArray];
+        let maxStringCount = 5; // Maximum number of strings in the string array starter
+        let maxIntCount = 5; // Maximum number of integers in the int array starter
+        let stringArray = skills.alphabetArray.sort(() => 0.5 - Math.random()).slice(0, maxStringCount); // Shuffle the alphabet array and take the first 5 letters for the string array starter
+        let intArray = Array.from({ length: maxIntCount }, () => Math.floor(Math.random() * 4) + 1); //Generate random integers between 1 and 5 for the int array starter
+        return [playerSkillsArray, stringArray, intArray];
+    },
+
+    determineTurnOrder: (player, enemy) => { // Return Structure: [ turnOrder, turnOrderMessage ]
+        player.spd = player.spd + Math.floor(Math.random() * 5) + 1; // Add a random value between 1 and 5 to the player's speed for turn order calculation
+        enemy.spd = enemy.spd + Math.floor(Math.random() * 5) + 1; // Add a random value between 1 and 5 to the enemy's speed for turn order calculation
+        if (player.spd > enemy.spd) {
+            return [0, `${player.name} is faster and will go first!`]; // Player goes first
+        } else if (enemy.spd > player.spd) {
+            return [1, `${enemy.name} is faster and will go first!`]; // Enemy goes first
+        } else {
+            const randomTurn = Math.round(Math.random()); // Randomly select who goes first if speeds are equal
+            return [randomTurn, `${player.name} and ${enemy.name} have equal speed! But ${randomTurn === 0 ? player.name : enemy.name} got the initiative and goes first!`];
+        }
+    },
+
+    planningPhase: (player, enemy, stringArray, intArray) => { // Returns [ player, enemy, stringArray, intArray, playerPlan, enemyPlan ]
+        let playerPlan;
+        let enemyPlan;
+        return [player, enemy, stringArray, intArray, playerPlan, enemyPlan]; 
+    },
+
+    
+    attackPhase: (turnOrder, player, enemy, stringArray, intArray, playerSkillOutput, enemySkillOutput) => {
+        let enemyAction = Math.round(Math.random() * 1);
+        if (enemyAction === 0) {
+            battleSystem.enemyTurn(0, player, enemy);
+        }
+        if (turnOrder === 0) {
+            if (enemyAction === 1) {
+               [ player, enemy ] = battleSystem.enemyTurn(1, player, enemy);
+            }
+        } else if (turnOrder === 1) {
+            if (enemyAction === 1) {
+               [ player, enemy ] = battleSystem.enemyTurn(1, player, enemy);
+            }
+    
+        }
+        return [player, enemy, stringArray, intArray];
     },
 
     playerTurn: (player, enemy, stringArray, intArray) => {
-        let output = [];
         const playerSkills = [
-            skills.concat(player, enemy, stringArray, intArray), 
-            skills.strToInt(player, enemy, stringArray, intArray), 
+            skills.strConcat(player, enemy, stringArray, intArray), 
             skills.strLength(player, enemy, stringArray, intArray), 
             skills.intToStr(player, enemy, stringArray, intArray), 
             skills.add(player, enemy, stringArray, intArray), 
@@ -37,35 +82,27 @@ const battleSystem = {
             skills.strDmg(player, enemy, stringArray, intArray)
         ];
         const playerSkillNames = skills.skillNames;
+        const playerSkillDescriptions = skills.skillDescriptions;
 
-        for (let i = 0; i < playerSkills.length; i++) {
-            BattleScreen.print(`${i + 1}. ${playerSkillNames[i]}`);
-        }
-        var skillChoice = prompt('Choose a skill by entering the corresponding number: ');
-        while (skillChoice < 1 || skillChoice > playerSkills.length) {
-            skillChoice = prompt('Invalid choice. Please enter a number corresponding to a skill: ');
-        };
-        output = playerSkills[skillChoice - 1];
-        return output;
 
     },
 
-    enemyTurn: (enemy, player) => {
-        let output = [player, enemy];
-
-        let enemyAction = Math.round(Math.random() * 1) < 1 ? "attack" : "defend";
-        if (enemyAction === "attack") {
-            BattleScreen.print(`${enemy.name} attacks ${player.name}!`);
+    enemyTurn: (moveID, player, enemy) => {
+        if (parseInt(moveID) === 1) {
+            console.log(`${enemy.name} attacks ${player.name}!`);
             const damage = Math.max(0, enemy.atk - player.def);
             player.hp -= damage;
-            BattleScreen.print(`${player.name} takes ${damage} damage! Remaining HP: ${player.hp}`);
+            console.log(`${player.name} takes ${damage} damage! Remaining HP: ${player.hp}`);
         } else {
-            BattleScreen.print(`${enemy.name} defends! Increased defense for this turn!`);
             enemy.status = "defending";
             enemy.def += 5*enemy.level; // Temporary defense boost for the turn
+            console.log(`${enemy.name} defends! Increased defense for this turn!`);
         }
+        return [player, enemy];
+    },
 
-        return output;
+    activateSkill: (player, enemy, stringArray, intArray, skillOutput) => {
+        
     },
 }
 
--- backEnd/skills.js
+++ backEnd/skills.js
@@ -1,94 +1,116 @@
 
 const skills = {
-    skillNames: ["concat", "strToInt", "strLength", "intToStr", "add", "multiply", "intDmg", "strDmg"],
-    skillDescriptions: [
-        "concat( string, string ) - Combines two strings into one.",
-        "strToInt( string ) - Converts a string to an integer (if possible).",
-        "strLength( string ) - Returns the length of a string and adds it to the int array.",
-        "intToStr( int ) - Converts an integer to a string and adds it to the string array.",
-        "add( int, int ) - Adds two integers into one.",
-        "multiply( int, int ) - Multiplies two integers into one.",
-        "intDmg( int ) - Deals damage based on an integer value.",
-        "strDmg( string ) - Deals damage based on the length of a string."
+    
+    alphabetArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    vowelArray: ["a", "e", "i", "o", "u"],
+
+    skillNames: [
+        "randStr()",
+        "randInt1To5()", 
+        "strConcat( string, string )", 
+        "strLength( string )", 
+        "intToStr( int )", 
+        "add( int, int )", 
+        "multiply( int, int )", 
+        "intDmg( int )",
+        "forEachInt( int[] )",
+        "strDmg( string )", 
+        "strArrayDmg( string[] )",
+        "forEachVowel( string[] )"
     ],
 
-    concat: (player, enemy, stringArray, intArray) => {
-        const output = [];
-        output.push(`concat( string, string )`);
-        output.push(`concat() function combines two strings.`);
-        output.push(`${player.name} used concat! Combined ${stringArray[0]} and ${stringArray[1]}!`);
-        output.push(`${stringArray[0] + stringArray[1]}`);
-        output.push('generate');
-        return output;
+    skillDescriptions: [
+        "type: Generate - Generates a random single letter string and adds it to the string array.",
+        "type: Generate - Generates a random integer (1-5) and adds it to the int array.",
+        "type: Generate - Combines two strings into one.",
+        "type: Generate - Returns the length of a string and adds it to the int array.",
+        "type: Generate - Converts an integer to a string and adds it to the string array.",
+        "type: Generate - Adds two integers into one.",
+        "type: Generate - Multiplies two integers into one.",
+        "type: AttackInt - Deals damage based on an integer value + attack stat.",
+        "type: AttackInt - Deals damage for each int in the int array multiplied by (40% of player's attack stat). (Warning: Consumes all int values)",
+        "type: AttackStr - Deals true damage based on the length of a string * player's attack stat.",
+        "type: AttackStr - Deals 40% of the length of the string array x attack stat as true damage. (Does not consume the string array or its contents)",
+        "type: AttackStr - Deals 150% of player's attack stat per vowel count inside the string array as true damage."
+    ],
+
+    // Skill Structure: [ skillID, skillType, skillOutputMessage, skillOutput ]
+    randStr: (player, enemy, stringArray, intArray) => {
+        const randomString = skills.alphabetArray[Math.floor(Math.random() * skills.alphabetArray.length-1)];
+        return [ 0, "Generate", `${player.name} used randStr! Generated a random string: ${randomString} and added it to the string array!`, randomString ];
     },
-    strToInt: (player, enemy, stringArray, intArray) => {
-        const output = [];
-        output.push(`strToInt( string )`);
-        output.push(`strToInt function converts string to int.`);
-        output.push(`${player.name} used strToInt! Converted ${stringArray[0]} to ${parseInt(stringArray[0])}!`);
-        output.push(`${parseInt(stringArray[0])}`);
-        output.push('generate');
-        return output;
+    randInt1To5: (player, enemy, stringArray, intArray) => {
+        const randomInt = Math.floor(Math.random() * 4) + 1;
+        return [ 1, "Generate", `${player.name} used randInt! Generated a random integer: ${randomInt} and added it to the int array!`, randomInt ];
+    },
+    strConcat: (player, enemy, stringArray, intArray) => {
+        if (stringArray.length > 2) {
+            return [ 2, "Generate", `${player.name} used strConcat! Combined ${stringArray[0]} and ${stringArray[1]} to create ${stringArray[0] + stringArray[1]}!`, stringArray[0] + stringArray[1] ];
+        }
     },
     strLength: (player, enemy, stringArray, intArray) => {
-        const output = [];
-        output.push(`strLength( string )`);
-        output.push(`strLength function creates a new int from the length of the string.`);
-        output.push(`${player.name} used strToInt! Created ${stringArray[0].length} int from ${stringArray[0]}!`);
-        output.push(`${stringArray[0].length}`);
-        output.push('generate');
-        return output;
+        if (stringArray.length > 0) {
+            return [ 3, "Generate", `${player.name} used strLength! Added the length of ${stringArray[0]} which is: ${stringArray[0].length} to Int Array!`, parseInt(stringArray[0].length) ];
+        }
     },
     intToStr: (player, enemy, stringArray, intArray) => {
-        const output = []
-        output.push(`intToStr( int )`);
-        output.push(`intToStr function converts int to string.`);
-        output.push(`${player.name} used intToStr! Converted ${intArray[0]} to ${intArray[0].toString()}!`);
-        output.push(`${intArray[0].toString()}`);
-        output.push('generate');
-        return output;
+        if (intArray.length > 0) {
+            return [ 4, "Generate", `${player.name} used intToStr! Converted "${intArray[0]}" to a string and added it to String Array!`, intArray[0].toString() ];
+        }
     },
-
     add: (player, enemy, stringArray, intArray) => {
-        const output = [];
-        output.push(`add( int, int )`);
-        output.push(`add function adds two int variables into one.`);
-        output.push(`${player.name} used add! Combined ${intArray[0]} and ${intArray[1]} to create ${intArray[0]+intArray[1]}!`);
-        output.push(`${intArray[0] + intArray[1]}`);
-        output.push('generate');
-        return output;
+        if (intArray.length > 1) {
+            return [ 5, "Generate", `${player.name} used add! Combined ${intArray[0]} and ${intArray[1]} to create ${parseInt(intArray[0]) + parseInt(intArray[1])}!`, parseInt(intArray[0]) + parseInt(intArray[1]) ];
+        }
     },
 
     multiply: (player, enemy, stringArray, intArray) => {
-        const output = [];
-        output.push(`multiply( int, int )`);
-        output.push(`multiply function multiplies two int variables into one.`);
-        output.push(`${player.name} used multiply! Multiplied ${intArray[0]} and ${intArray[1]} to create ${intArray[0]*intArray[1]}!`);
-        output.push(`${intArray[0] * intArray[1]}`);
-        output.push('generate');
-        return output;
+        if (intArray.length > 1) {
+            return [ 6, "Generate", `${player.name} used multiply! Multiplied ${intArray[0]} and ${intArray[1]} to create ${parseInt(intArray[0]) * parseInt(intArray[1])}!`, parseInt(intArray[0]) * parseInt(intArray[1]) ];
+        }
     },
 
     intDmg: (player, enemy, stringArray, intArray) => {
-        const output = []
-        output.push(`intDmg()`)
-        output.push(`intDmg function deals dmg based on the int given.`)
-        output.push(`${player.name} used intDmg! Dealt ${intArray[0]} damage to ${enemy.name}!`)
-        output.push(`${intArray[0]}`)
-        output.push('attack')
-        return output
+        if (intArray.length > 0) {
+            return [ 7, "Attack", `${player.name} used intDmg! Dealt ${intArray[0]} damage to ${enemy.name}!`, parseInt(intArray[0]) + parseInt(player.atk) ];
+        }
     },
 
-    strDmg: (player, enemy, stringArray, intArray) => {
-        const output = []
-        output.push(`strDmg()`)
-        output.push(`strDmg function deals dmg based on the length of the string given.`)
-        output.push(`${player.name} used strDmg! Dealt ${stringArray[0].length} damage to ${enemy.name}!`)
-        output.push(`${stringArray[0].length}`)
-        output.push('attack')
-        return output
-    }
+    forEachIntDmg: (player, enemy, stringArray, intArray) => {
+        if (intArray.length > 0) {
+            let totalDmg = 0;
+            for (let i = 0; i < intArray.length; i++) {
+                totalDmg += (parseInt(intArray[i])*0.3) + parseInt(player.atk);
+            }
+            return [ 8, "Attack", `${player.name} used forEachInt! Dealt ${totalDmg} true damage to ${enemy.name}!`, totalDmg ];
+        }
+    },
 
+
+    strDmg: (player, enemy, stringArray, intArray) => {
+        if (stringArray.length > 0) {
+            return [ 9, "Attack", `${player.name} used strDmg! Dealt ${parseInt(stringArray[0].length)*parseInt(player.atk)} true damage to ${enemy.name}!`, parseInt(stringArray[0].length)*parseInt(player.atk) ];
+        }
+    },
+
+    strArrayDmg: (player, enemy, stringArray, intArray) => {
+        if (stringArray.length > 0) {
+            return [ 10, "Attack", `${player.name} used strArrayDmg! Dealt ${0.4 * (parseInt(stringArray.length)*parseInt(player.atk))} true damage to ${enemy.name}!`, 0.4 * (parseInt(stringArray.length)*parseInt(player.atk)) ];
+        }
+    },
+
+    forEachVowelDmg: (player, enemy, stringArray, intArray) => {
+        if (stringArray.length > 0) {
+            let totalDmg = 0;
+            for (let i = 0; i < stringArray.length; i++) {
+                if (skills.vowelArray.includes(stringArray[i].toLowerCase())) {
+                    totalDmg += 1.5 * player.atk;
+                }
+            }
+            return [ 11, "Attack", `${player.name} used forEachVowel! Dealt ${totalDmg} true damage to ${enemy.name}!`, totalDmg ];
+        }
+        
+    },
 }
 
 export default skills;
